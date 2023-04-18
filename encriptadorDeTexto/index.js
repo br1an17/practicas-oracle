32dc9d7000c1ce7bb55c6741d50906444057126b
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-04-18 19:05:47 -0300

    se me borraron los cambios

--- encriptadorDeTexto/index.js
+++ encriptadorDeTexto/index.js
@@ -1,8 +1,28 @@
+const texto = document.getElementById("textoE")
+const textoCopiar = document.getElementById("textoC")
+const botonCopiar = document.getElementById("copiar")
+const botonEncriptar = document.getElementById("encriptar")
+const botonDesencriptar = document.getElementById("desencriptar")
+
+const encriptar = (e) =>  {
+e.preventDefault( console.log(texto.value))
+textoCopiar.value = texto.value
+texto.value = ""
+}
+const desencriptar = (e) =>  {
+e.preventDefault( console.log(texto.value))
+textoCopiar.value = texto.value
+}
+
+
+const copiar = (e) =>  {
+    e.preventDefault( console.log(texto.value))
+    textoCopiar.value = texto.value
+
+    }
 
 
 
-
-const capturar = document.querySelector("textarea")
-
-console.log(capturar.value)
-
+botonEncriptar.onclick = encriptar
+botonDesencriptar.onclick = desencriptar
+botonCopiar.onclick = copiar

788e4f4e1c4b78f673dc57f34eb030b363ee77ee
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-04-20 21:51:48 -0300

    funciones

--- encriptadorDeTexto/index.js
+++ encriptadorDeTexto/index.js
@@ -26,80 +26,49 @@
 // botonEncriptar.onclick = encriptar
 // botonDesencriptar.onclick = desencriptar
 // botonCopiar.onclick = copiar
-function validarTexto (texto) {
-    
-    let caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
-    let mayusculas = /[A-Z]/g;  
-    let vacio="";  
-      
-    if(texto.match(mayusculas)||texto.match(caracteres)){
-        alert("No se permiten caracteres especiales ni mayusculas");
-        return true; 
-    }else if(texto==vacio){
-       alert("Ingrese un mensaje para encriptar");
-        return true;
-   }else {
-        return false;
-    }
+function copiar() {
+    navigator.clipboard.writeText(
+        document.getElementById("encrypted-text").value
+    );
+}
+document.querySelector("#boton-copiar").addEventListener("click", copiar);
+
+
+function desencriptar() {
+    var texto = document.querySelector("#input-text").value;
+    texto = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+    document.getElementById("encrypted-text").value = texto;
 }
 
-
-let btnEncriptar = document.querySelector("#btn-encriptar");
-
-btnEncriptar.addEventListener("click",function ()  {
-    let textInput = document.querySelector("#input-texto").value;
-    let textoIngresado = textInput;
-   
-    if (validarTexto (textoIngresado) == false) {       
-        let Encriptado = encriptar(textoIngresado);
-        let resultado = document.querySelector("#msg");
-        resultado.value = Encriptado;
-    } else {        
-        textInput = "";     
-     
+var boton = document.getElementById("boton-desencriptar");
+boton.onclick = function () {
+    if(document.getElementById("input-text").value.match(/^[a-z ]*$/)) {
+        desencriptar();
+    } else {
+        alert("Solo se permiten letras minusculas, sin caracteres especiales.")
     }
-               
-})
-
-
-const reglas = { "e":"enter","i":"imes","a":"ai","o":"ober","u":"ufat"};
-
-function encriptar (textoIngresado) {
-    let Encriptado = "";
-    for (const obj in reglas) {
-        Encriptado = textoIngresado.replaceAll(obj,reglas[obj]);
-        textoIngresado = Encriptado;        
-    }
-    return (Encriptado);
+};
+function encriptar() {
+    var texto = document.getElementById("input-text").value;
+    texto = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+    document.getElementById("encrypted-text").value = texto;
 }
 
-
-let btnCopiar = document.querySelector("#btn-copy");
-
-btnCopiar.addEventListener("click",function(){        
-    let Copiado = document.querySelector("#msg").value;
-    navigator.clipboard.writeText(Copiado);
-    document.querySelector("#input-texto").value="";
-
-});
-
-let btnDesencriptar = document.querySelector("#btn-desencriptar");
-
-btnDesencriptar.addEventListener("click", function(){
-    let textoIngresado = document.querySelector("#input-texto").value;
-    let Desencriptado = desencriptar(textoIngresado);
-
-    let resultado = document.querySelector("#msg");
-    resultado.value = Desencriptado;
-})
-
-
-
-function desencriptar (textoIngresado) {
-    let Encriptado = "";
-    for (const obj in reglas) {
-        Encriptado = textoIngresado.replaceAll(reglas[obj],obj);
-        textoIngresado = Encriptado;        
+var boton = document.getElementById("boton-encriptar");
+boton.onclick = function () {    
+    if(document.getElementById("input-text").value.match(/^[a-z ]*$/)) {
+        encriptar();
+    } else {
+        alert("Solo se permiten letras minusculas, sin caracteres especiales.")
     }
-    return (Encriptado);
-}
+};

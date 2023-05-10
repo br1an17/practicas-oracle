6440c43a4db8a4f53662827b3e729d5fb852032d
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-05-09 21:36:55 -0300

    preparando los cambios

--- encriptadorDeTexto/index.js
+++ encriptadorDeTexto/index.js
@@ -1,31 +1,4 @@
-// const texto = document.getElementById("textoE")
-// const textoCopiar = document.getElementById("textoC")
-// const botonCopiar = document.getElementById("copiar")
-// const botonEncriptar = document.getElementById("encriptar")
-// const botonDesencriptar = document.getElementById("desencriptar")
 
-// const encriptar = (e) =>  {
-// e.preventDefault( console.log(texto.value))
-// textoCopiar.value = texto.value
-// texto.value = ""
-// }
-// const desencriptar = (e) =>  {
-// e.preventDefault( console.log(texto.value))
-// textoCopiar.value = texto.value
-// }
-
-
-// const copiar = (e) =>  {
-   
-//     textoCopiar.value = texto.value
-
-// e.preventDefault( console.log(textoCopiar.value)) }
-
-
-
-// botonEncriptar.onclick = encriptar
-// botonDesencriptar.onclick = desencriptar
-// botonCopiar.onclick = copiar
 function copiar() {
     navigator.clipboard.writeText(
         document.getElementById("encrypted-text").value

eb81267ce83f420dac4ecbf49e2bdfd7b7e5a674
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-05-18 16:28:39 -0300

    cambios

--- encriptadorDeTexto/index.js
+++ encriptadorDeTexto/index.js
@@ -1,50 +1,4 @@
 
-// function copiar() {
-//     navigator.clipboard.writeText(
-//         document.getElementById("encrypted-text").value
-//     );
-// }
-// document.querySelector("#boton-copiar").addEventListener("click", copiar);
-
-
-// function desencriptar() {
-//     var texto = document.querySelector("#input-text").value;
-//     texto = texto
-//         .replace(/enter/gi, "e")
-//         .replace(/imes/gi, "i")
-//         .replace(/ai/gi, "a")
-//         .replace(/ober/gi, "o")
-//         .replace(/ufat/gi, "u");
-//     document.getElementById("encrypted-text").value = texto;
-// }
-
-// var boton = document.getElementById("boton-desencriptar");
-// boton.onclick = function () {
-//     if(document.getElementById("input-text").value.match(/^[a-z ]*$/)) {
-//         desencriptar();
-//     } else {
-//         alert("Solo se permiten letras minusculas, sin caracteres especiales.")
-//     }
-// };
-// function encriptar() {
-//     var texto = document.getElementById("input-text").value;
-//     texto = texto
-//         .replace(/e/gi, "enter")
-//         .replace(/i/gi, "imes")
-//         .replace(/a/gi, "ai")
-//         .replace(/o/gi, "ober")
-//         .replace(/u/gi, "ufat");
-//     document.getElementById("encrypted-text").value = texto;
-// }
-
-// var boton = document.getElementById("boton-encriptar");
-// boton.onclick = function () {    
-//     if(document.getElementById("input-text").value.match(/^[a-z ]*$/)) {
-//         encriptar();
-//     } else {
-//         alert("Solo se permiten letras minusculas, sin caracteres especiales.")
-//     }
-// };
 const inputTexT = document.getElementById('input--text');
 const btnEncoder = document.getElementById('btn--encoder');
 const btnDecoder = document.getElementById('btn--decoder');
@@ -109,12 +63,12 @@ btnCopy.addEventListener('click', (e) => {
     setTimeout(() => {
     navigator.clipboard.writeText(message.value)
         .then(() => {
-             copy.textContent = "Se ha copiado exitosamente! 😉";
+             copy.textContent = "Se ha copiado exitosamente!";
              setTimeout(() => window.location.reload(), 1500)
              
             })
             .catch(() => {
-                copy.textContent = "Algo salió mal! 😔";
+                copy.textContent = "Algo salió mal!";
             });  
         }, 1000)
     navigator.reset();    
